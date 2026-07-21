a115e59f2d50318cc4ea9490bb6b292eee7e0b88
npoway <n.smoliar2013@gmail.com>
2026-07-21 16:00:03 +0300

    Initial Commit

---
+++ ChikenPark/ChikenParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikenParkApp.swift
+//  ChikenPark
+//
+//  Created by Никита on 21.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChikenParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
